c0eca0e090b937d6f4fc3ab070b6fe16879cb343
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-19 19:11:32 -0400

    v1.1

--- prime_guesser.py
+++ prime_guesser.py
@@ -1,6 +1,6 @@
 '''
 Author: Andrew Lounsbury
-Date: 1/3/23
+Date: 3/1/23
 Purpose: prime number guessing game
 '''
 import math
@@ -9,13 +9,13 @@ import random
 def prime(n):
     if n == 1:
         return False
-
-    for i in range(2, math.ceil(math.sqrt(n))):
+    
+    for i in range(2, math.ceil(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
 
-Max = input("Enter a maximum number: ")
+Max = input("Enter a maximum number to set the difficulty: ")
 while not Max.isdigit():
     Max = input("The maximum must be a positive integer: ")
 Max = int(Max)
@@ -29,11 +29,15 @@ while True:
     random_number = random.randrange(1, Max)
     while random_number in played_before:
         random_number = random.randrange(1, Max)
-    guess = input("Is " + str(random_number) + " prime or composite? Press q to quit ").lower()
+    guess = input("Is " + str(random_number) + " prime (p) or composite (c)? Press q to quit ").lower()
+    
+    while guess != "p" and guess != "c" and guess != "q":
+        print("Invalid input")
+        guess = input("Is " + str(random_number) + " prime (p) or composite (c)? Press q to quit ").lower()
     
     if guess == "q":
         break
-    if guess == "prime" and prime(random_number) or guess == "composite" and not prime(random_number):
+    if guess == "p" and prime(random_number) or guess == "c" and not prime(random_number):
         print("You got it right!")
         correct_guesses += 1
         played_before.append(random_number)
