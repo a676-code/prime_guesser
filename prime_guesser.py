c541d4bfea67621954bbf421f196bec1c884f4a9
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-29 13:18:16 -0400

    update main script

--- prime_guesser.py
+++ prime_guesser.py
@@ -10,7 +10,7 @@ def prime(n):
     if n == 1:
         return False
     
-    for i in range(2, math.ceil(math.sqrt(n)) + 1):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
